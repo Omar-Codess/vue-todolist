e819b8680e6ee2d3ab28eba22f315b59889ee4c9
Omar-Codess <omartaki8@gmail.com>
2022-10-27 16:45:31 +0200

    creata base html e css, e array di oggetti

--- js/script.js
+++ js/script.js
@@ -3,7 +3,19 @@ const { createApp } = Vue;
 createApp({
     data() {
         return{
+            todoList: [
+                {
+                    text: "fare l'esercizio con vue",
+                    done: false
+                },
+                {
+                    text: "allenarmi prima delle otto",
+                    done: true
+                },
+            ],
+        };
+    },
+    methods: {
 
-        }
     }
 }).mount("#app")
