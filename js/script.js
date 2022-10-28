cf5c85d246cfb45bf0813a451fe5f607c57ba6b5
Omar-Codess <omartaki8@gmail.com>
2022-10-28 18:01:41 +0200

    completata correzione

--- js/script.js
+++ js/script.js
@@ -1,22 +1,79 @@
+// const { createApp } = Vue;
+
+// createApp({
+//     data() {
+//         return{
+//             currentItem: 0,
+//             toDoList: [
+//                 {
+//                     text: "fare l'esercizio con vue",
+//                     done: false
+//                 },
+//                 {
+//                     text: "allenarmi prima delle otto",
+//                     done: true
+//                 },
+//             ],
+//         };
+//     },
+//     methods: {
+
+//     }
+// }).mount("#app");
+
+// CORREZIONE
+
 const { createApp } = Vue;
 
 createApp({
-    data() {
-        return{
-            currentItem: 0,
-            toDoList: [
-                {
-                    text: "fare l'esercizio con vue",
-                    done: false
-                },
-                {
-                    text: "allenarmi prima delle otto",
-                    done: true
-                },
-            ],
-        };
+  data() {
+    return {
+      newTodo: "",
+      hasError: false,
+      todos: [
+        {
+          text: "Fare la spesa",
+          done: false,
+        },
+        {
+          text: "Fare l'esercizio pomeridiano",
+          done: false,
+        },
+        {
+          text: "Dare da mangiare alla gatta",
+          done: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    removeTodo(todoIndex) {
+      this.todos.splice(todoIndex, 1);
     },
-    methods: {
+    addNewTodo() {
+      if (this.newTodo.length > 5) {
+        // Aggiungo il nuovo todo
+        const todo = {
+          text: this.newTodo,
+          done: false,
+        };
+        this.todos.push(todo);
+        // this.todos.push({
+        //   text: this.newTodo,
+        //   done: false,
+        // });
 
-    }
+        // Ripulisco l'input del todo
+        this.newTodo = "";
+        this.hasError = false;
+      } else {
+        this.hasError = true;
+      }
+    },
+    changeDone(todoIndex) {
+      // this.todos[todoIndex].done = !this.todos[todoIndex].done;
+      const clickedTodo = this.todos[todoIndex];
+      clickedTodo.done = !clickedTodo.done;
+    },
+  },
 }).mount("#app");
